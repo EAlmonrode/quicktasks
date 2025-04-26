9302c1ac2b778e3d01f563d279b343268d9883f7
Ethan Almonrode <ealmonrode@uiowa.edu>
2025-04-25 19:22:40 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb+srv://ealmonrode:Kalafra@cluster0.s5u716e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # replace this!
+MONGO_URL = "mongodb+srv://ealmonrode:Kalafra@cluster0.s5u716e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.quicktasks
